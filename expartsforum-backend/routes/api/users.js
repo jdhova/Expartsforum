e95094c29b32e8e90322558c57a80704a3963b96
Jude OkAGU <juud0010@gmail.com>
2019-03-05 11:42:41 +0100

    first

--- expartsforum-backend/routes/api/users.js
+++ expartsforum-backend/routes/api/users.js
@@ -8,6 +8,10 @@ const keys =  require('../../config/keys');
  const passport = require('passport')
 // const passport = require('../config/passport')
 
+
+//Load imput Validation
+const  validateRegisterInput = require ('../../validation/register')
+
 // Load user model
 const User = require('../../models/User');
 
@@ -15,6 +19,15 @@ const User = require('../../models/User');
 router.get('/test', (req,res) => res.json({msg: 'user works'}))
 
 router.post('/register', (req, res) => {
+
+    //  res.json({msg: 'user works'})
+    //  })
+    const { erros, isValid }  = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(console.log ('ERRORRR',errors))
+    }
+
     User.findOne({email: req.body.email})
     .then(user => {
         if(user) {
@@ -34,7 +47,7 @@ router.post('/register', (req, res) => {
 
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err,hash) => {
-                    if (err) throw err;
+                    // if (err) throw err;
                     newUser.password = hash;
                     newUser.save()
                     .then(user => res.json(user))
--- expartsforum-backend/validation/register.js
+++ expartsforum-backend/validation/register.js
@@ -1,14 +1,15 @@
-const validator = require ('validator');
+const Validator = require ('validator');
+const isEmpty = require  ('./is-empty');
 
 
 module.exports = function validatateRegisterInput(data) {
     let errors = {};
 
     if (!Validator.isLength(data.name, {min: 2, max: 30})){
-        errors.name ='Name should between 2 and 30 characters';
+        errors.name ='Name must be between 2 and 30 characters';
     }
     return {
         erros,
-        isValid : errors
+        isValid : isEmpty(errors)
     }
 }

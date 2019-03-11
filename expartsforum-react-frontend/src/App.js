c07a565e7def7d7d80390f95515c53409a2f1da6
Jude OkAGU <juud0010@gmail.com>
2019-03-11 18:20:10 +0100

    ligin react router done

--- expartsforum-react-frontend/src/App.js
+++ expartsforum-react-frontend/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from './components/auth/register'
+import Login from './components/auth/login'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>welcome to react</h1>
-      </div>
+      <Router>
+          <div className="App">
+          <Navbar/>
+          <div className ="container"/>
+          <Route exact path = "/"  component={ Landing }/>
+          <Route exact path = "/login" component={Login}/>
+          <Route exact path = "/register" component={Register}/>
+          {/* <Footer /> */}
+        </div>
+      </Router>
     );
   }
 }
---
+++ expartsforum-react-frontend/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+ function Footer() {
+  return (
+        <div>  
+          <footer className="bg-dark text-white mb-95 p-4 text-center">
+                Copyright &copy; {new Date().getFullYear()} Exparts Forum
+            </footer>
+        </div>
+  )
+}
+
+export default Footer

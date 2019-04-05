d9a7278f98bc03848c4bcea5010225a45bbd48c3
Jude OkAGU <juud0010@gmail.com>
2019-04-05 11:25:04 +0200

    test

--- expartsforum-backend/index.js
+++ expartsforum-backend/index.js
@@ -58,14 +58,14 @@ app.use('/api/employer',employer) // employer route
 app.use('/api/image', image)
 app.use('/api/profilee', profilee)
 
-// Serve static accet
+// Serve static asset
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
-    })
+    // app.get('*', (req,res) => {
+    //     res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
+    // })
 }
 
 const port = process.env.PORT || 3001;

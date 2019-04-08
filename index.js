fa386df44fe9bc2b49e26e18bd05d88d6ca4f7ff
Jude OkAGU <juud0010@gmail.com>
2019-04-08 19:58:19 +0200

    done new

--- index.js
+++ index.js
@@ -47,9 +47,7 @@ app.use(passport.initialize());
 
 
 
-// app.get('/', (req, res) =>{
-// res.send('hello world')
-// });
+
 
 app.use('/api/post', post);
 app.use('/api/profiles', profiles);
@@ -60,13 +58,23 @@ app.use('/api/profilee', profilee)
 
 // Serve static asset
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
-    })
-}
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
+//     })
+// }
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Back end server running on port ${port}`))
